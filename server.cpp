9397f213d07cf22a64477a8891de0def5d4900e5
Лев Воробьев <lev.vorobjev@rambler.ru>
2019-02-12 06:10:10 +0300

    Tftps

--- server.cpp
+++ server.cpp
@@ -15,65 +15,59 @@
 #include <arpa/inet.h>
 #typedef int SOCKET
 #endif
+#include <stdexcept>
+using namespace std;
 
 #define BUFFER_SIZE 512
 #define TFTP_PORT 8969
 
 class tftp {
 private:
-    int status = 0;
     int state = 0;
     SOCKET sock = INVALID_SOCKET;
 
 public:
-    int getStatus() const;
-
     void start();
 
+	~tftp() noexcept {
+		cleanup();
+	}
+
 protected:
-    void cleanup();
+    void cleanup() noexcept;
 };
 
 int main(int argc, char* argv[]) {
     tftp server;
-    server.start();
-    return server.getStatus();
+	try {
+		server.start();
+	} catch(const logic_error& ex) {
+		printf("%s: %d\n", ex.what(), error);
+		return EXIT_FAILURE;
+	}
+    return EXIT_SUCCESS;
 }
 
 void tftp::start() {
     char buf[BUFFER_SIZE];
 #ifdef _WIN32
     WSADATA wsd{0};
-    if (WSAStartup(MAKEWORD(2,2), &wsd) != 0) {
-        status = 1;
-        return;
-    }
+    if (WSAStartup(MAKEWORD(2,2), &wsd) != 0)
+        throw logic_error("startup fail");
     state = 1;
 #endif
     sockaddr_in addr{AF_INET};
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(TFTP_PORT);
     sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sock == INVALID_SOCKET) {
-        printf("socket error: %d\n", error);
-        status = 1;
-        cleanup();
-        return;
-    }
+    if (sock == INVALID_SOCKET)
+        throw logic_error("socket error");
     state = 2;
-    if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
-        printf("bind failed: %d\n", error);
-        status = 1;
-        cleanup();
-        return;
-    }
+    if (bind(sock, (sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR)
+        throw logic_error("bind failed");
     BOOL is_broadcast = TRUE;
-    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&is_broadcast, sizeof(is_broadcast)) == SOCKET_ERROR) {
-        printf("option setting error: %d\n", error);
-        status = 1;
-        cleanup();
-        return;
-    }
+    if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&is_broadcast, sizeof(is_broadcast)) == SOCKET_ERROR)
+        throw logic_error("option setting error");
     static sig_atomic_t active = true;
     signal(SIGINT, [](int s) { active = false; });
 
@@ -82,21 +76,13 @@ void tftp::start() {
     do {
         int n = recvfrom(sock, buf, BUFFER_SIZE, 0,
                          (sockaddr*)&client, &length);
-        if (n == SOCKET_ERROR) {
-            printf("recv failed: %d\n", error);
-            status = 1;
-            cleanup();
-            break;
-        }
+        if (n == SOCKET_ERROR)
+            throw logic_error("receive failed");
         printf("%s\n", buf);
     } while(active);
 }
 
-int tftp::getStatus() const {
-    return status;
-}
-
-void tftp::cleanup() {
+void tftp::cleanup() noexcept {
     printf("Bue!\n");
     switch(state) {
         case 2:
