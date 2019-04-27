ff0207f63000112c1d3373ef57d9b6dad8ccde0b
Лев Воробьев <lev.vorobjev@rambler.ru>
2019-04-27 21:07:37 +0300

    Upd

--- send_dialog.cpp
+++ send_dialog.cpp
@@ -13,12 +13,14 @@ tftp::send_dialog::send_dialog() {
         list1.assign(this, IDC_SEND);
         list1.imageList16.load_from_resource(IDI_ICON1);
         list1.imageList32.load_from_resource(IDI_ICON1);
-        list1.columns.add(L"Host", 125);
+        list1.columns.add(L"Название", 125);
+        list1.columns.add(L"IP Адрес", 90);
         return 0;
     });
 
     on_command(IDOK, [this](wm::command p)->INT_PTR {
-        list1.items.add(L"Added");
+        list1.items.add(L"localhost");
+        list1.items.get_last().set_text(L"127.0.0.1",1);
         return 0;
     });
 }
